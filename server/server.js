ef87ba5fade328256c605d1c9d04639fc005cb44
Sowmya Dasari <161193091+sowmyadasar1@users.noreply.github.com>
2026-03-12 04:02:24 +0530

    Remove specific CORS origin restriction

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ if (!admin.apps.length) {
 const db = admin.firestore();
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
